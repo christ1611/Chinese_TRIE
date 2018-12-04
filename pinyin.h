ba457944d1044f2890691fcdcfd0b699478f1e15
christ1611 <42228029+christ1611@users.noreply.github.com>
2018-12-04 15:31:58 +0900

    Update pinyin.h

--- pinyin.h
+++ pinyin.h
@@ -1,24 +1,17 @@
 /*
 #####################################################################################
 #Program name  : TRIE model for chineze-Hanzi Pinyin Prediction
-#Description   : Make the TRIE based on chinese Pinyin character
-#                Real time training model of chinese-hanzi prediction model
-#                Training The TRIE to understand the hanzi-Pinyin Relation
-#                Training the TRIE to give the prediction for the next given hanzi
-#                Training the TRIE to give the prediction based on the uncompleted pinyin
-#                   (ex:nih -> 你好/nihao)
-#                Training the TRIE to give the prediction based on given consonant only
-#                   (ex:ALBB -> 阿里巴巴(Alibaba))
-#                Build a adaptive TRIE Initizilation based on the Sogou dataset
-#                Updating the TRIE and reccomendation score based on the user input or preferences
-#                Weighting and sorting the hanzi prediction
+#Description   : All the library that needed in pinyin.cpp
 #
 #Author        : Hotama Christianus Frederick
-#Project       : SLM - 논리 고도화 (redmine 10054,10095)
-#Version       : v2.0
-#Update        : The TRIE Scoring system
+#Commit date   : December 2018
 ######################################################################################
 */
+
+#include <unistd.h>
+#ifndef SRC_PINYIN_H_
+#define SRC_PINYIN_H_
+
 #include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
@@ -44,3 +37,7 @@
 #define GetCurrentDir getcwd
 #endif
 using namespace std;
+
+
+
+#endif
